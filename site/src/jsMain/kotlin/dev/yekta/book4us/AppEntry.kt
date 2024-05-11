a9963b2502ae495d8e3ec10bde00e8c0bf316491
Ali Khaleqi Yekta <me@yekta.dev>
2024-05-11 20:38:05 +0330

    fix(AppEntry): Force light mode

--- site/src/jsMain/kotlin/dev/yekta/book4us/AppEntry.kt
+++ site/src/jsMain/kotlin/dev/yekta/book4us/AppEntry.kt
@@ -1,7 +1,6 @@
 package dev.yekta.book4us
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import com.varabyte.kobweb.compose.css.ScrollBehavior
 import com.varabyte.kobweb.compose.ui.modifiers.minHeight
 import com.varabyte.kobweb.compose.ui.modifiers.scrollBehavior
@@ -13,25 +12,17 @@ import com.varabyte.kobweb.silk.components.style.toModifier
 import com.varabyte.kobweb.silk.init.InitSilk
 import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import kotlinx.browser.localStorage
 import org.jetbrains.compose.web.css.vh
-
-private const val COLOR_MODE_KEY = "book4us:colorMode"
 
 @InitSilk
 fun initColorMode(ctx: InitSilkContext) {
-    ctx.config.initialColorMode = localStorage.getItem(COLOR_MODE_KEY)?.let { ColorMode.valueOf(it) } ?: ColorMode.DARK
+    ctx.config.initialColorMode = ColorMode.LIGHT
 }
 
 @App
 @Composable
 fun AppEntry(content: @Composable () -> Unit) {
     SilkApp {
-        val colorMode = ColorMode.current
-        LaunchedEffect(colorMode) {
-            localStorage.setItem(COLOR_MODE_KEY, colorMode.name)
-        }
-
         Surface(
             SmoothColorStyle.toModifier()
                 .minHeight(100.vh)
